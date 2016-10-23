3c773f561879877c071777ff1d1b50b3c99bba91
Febrinanda Endriz Pratama <febriinanda@gmail.com>
2016-10-23 14:04:07 +0700

    Add static header

--- App.jsx
+++ App.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 class App extends React.Component {
    constructor(props) {
       super(props);
-	
+      
    }
-	
+
    render() {
       return (
          <div>
-            <Header headerProp = "Eternity"/>
-            <Content contentProp = "Fate"/>
+            <Header />
          </div>
       );
    }
@@ -20,20 +19,52 @@ class Header extends React.Component {
    render() {
       return (
          <div>
-            <h1>{this.props.headerProp}</h1>
-         </div>
+            <div className="ui menu">
+                <ProductBrand brandName="Our Company"/>
+                <MenuItem itemName="Home" />
+                <MenuItem itemName="Job" />
+                <MenuItem itemName="Location" />
+            </div>
+        </div>
       );
    }
+}
+
+class MenuItem extends React.Component{
+    render(){
+        return (
+            <a className="item">{this.props.itemName}</a>
+        );
+    }
 }
 
 class Content extends React.Component {
    render() {
       return (
          <div>
-            <h2>{this.props.contentProp}</h2>
+            
          </div>
       );
    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
+
+class ProductBrand extends React.Component {
+    render () {
+        return (
+            <div className="header item">
+                {this.props.brandName}                
+            </div>
+        )
+    }
 }
 
 export default App;
